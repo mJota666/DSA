f25abdc6855f960c2327785b15c3b710834950af
NPT0116 <thanh1612004@gmail.com>
2023-07-19 10:25:46 +0700

    fix bubble

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -10,6 +10,7 @@ void bubble_sort_comp(int *arr, int n, long long &count_comp)
     for (i = 0; i < n - 1; i++)
     {
         // swapped = false;
+        count_comp +=n - i - 1;
         for (j = 0; j < n - i - 1; j++)
         {
             if (++count_comp && arr[j] > arr[j + 1])
@@ -17,14 +18,16 @@ void bubble_sort_comp(int *arr, int n, long long &count_comp)
                 swap(arr[j], arr[j + 1]);
                 // swapped = true;
             }
+            
         }
+        
         /*
          đây là bước cải tiến nếu không có phần tử nào được swap thì nó sẽ break
               if (swapped == false)
                   break;
          */
     }
-    count_comp += (n - 1) * (n - 1);
+    count_comp += n-1;
 }
 void bubble_sort_time(int *arr, int n, long double &time)
 {
