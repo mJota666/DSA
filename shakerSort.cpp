e09eedde02345e6f8ed02aeaeb62660ec0dbaf1e
NPT0116 <thanh1612004@gmail.com>
2023-07-27 14:32:32 +0700

    sua shaker

--- shakerSort.cpp
+++ shakerSort.cpp
@@ -1,39 +1,31 @@
 #include "shakerSort.h"
-
-/*
- https://www.youtube.com/watch?v=FgExRtl66Lc
- */
-
-void shaker_sort_comp(int *a, int n, long long &count_comp)
+void shaker_sort_comp(int a[], int n, long long &count_comp)
 {
-    int left = 0;
-    int right, mark;
-    // mark được dùng để đánh dấu chuỗi đã được duyệt qua và có thứ tự rồi.
-    right = mark = n - 1;
-
-    while (++count_comp && left < right)
+    int Left = 0;
+    int Right = n - 1;
+    int k = 0;
+    while (++count_comp&&Left < Right)
     {
-        for (int i = right; i > left; i--)
+        for (int i = Left; i < Right; i++)
         {
-            if (++count_comp && a[i] < a[i - 1])
+            if (++count_comp&&(a[i] > a[i + 1]))
+            {
+                swap(a[i], a[i + 1]);
+                k = i;
+            }
+        }
+        count_comp += (Right - Left + 1);
+        Right = k;
+        for (int i = Right; i > Left; i--)
+        {
+            if (++count_comp&&(a[i] < a[i - 1]))
             {
                 swap(a[i], a[i - 1]);
-                mark = i;
+                k = i;
             }
         }
-        count_comp += (right - left + 1);
-        left = mark;
-
-        for (int j = left; j > right; j++)
-        {
-            if (++count_comp && a[j] < a[j + 1])
-            {
-                swap(a[j], a[j + 1]);
-                mark = j;
-            }
-        }
-        count_comp += (right - left + 1);
-        right = mark;
+        count_comp += (Right - Left + 1);
+        Left = k;
     }
 }
 void shaker_sort_time(int *a, int n, long double &time)
@@ -41,33 +33,30 @@ void shaker_sort_time(int *a, int n, long double &time)
     time = 0;
     clock_t start, end;
     start = clock();
-    int left = 0;
-    int right, mark;
-    // mark được dùng để đánh dấu chuỗi đã được duyệt qua và có thứ tự rồi.
-    right = mark = n - 1;
-
-    while (left < right)
-    {
-        for (int i = right; i > left; i--)
+    int Left = 0;
+        int Right = n - 1;
+        int k = 0;
+        while (Left < Right)
         {
-            if (a[i] < a[i - 1])
+            for (int i = Left; i < Right; i++)
             {
-                swap(a[i], a[i - 1]);
-                mark = i;
+                if (a[i] > a[i + 1])
+                {
+                    swap(a[i], a[i + 1]);
+                    k = i;
+                }
             }
-        }
-        left = mark;
-
-        for (int j = left; j < right; j++)
-        {
-            if (a[j] > a[j + 1])
+            Right = k;
+            for (int i = Right; i > Left; i--)
             {
-                swap(a[j], a[j + 1]);
-                mark = j;
+                if (a[i] < a[i - 1])
+                {
+                    swap(a[i], a[i - 1]);
+                    k = i;
+                }
             }
+            Left = k;
         }
-        right = mark;
-    }
     end = clock();
     time = (double)(end - start) / CLOCKS_PER_SEC;
 }
